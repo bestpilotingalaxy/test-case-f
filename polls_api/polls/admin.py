b858eb47c9a9a23e56662f083e7c0e7a4438cef1
bestpilotingalaxy <egorlaptev98@gmail.com>
2020-11-12 20:59:48 +0300

    Models and admin actions done

--- polls_api/polls/admin.py
+++ polls_api/polls/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Poll, Question, Answer
 
-# Register your models here.
+
+class QuestionInline(admin.TabularInline):
+    """
+    Inline representation for questions.
+    """
+    model = Question
+
+
+class PollAdmin(admin.ModelAdmin):
+    """
+    Adds action which create new questions in Poll admin panel.
+    """
+    inlines = [
+        QuestionInline,
+    ]
+    # block start_date field for changing after object creation
+    # readonly_fields = ('start_date', )
+    
+    class Meta:
+        model = Poll
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
--- polls_api/polls/models.py
+++ polls_api/polls/models.py
@@ -1,1 +1,62 @@
-from django.db import models
+from django.db import models as m
+
+
+class Poll(m.Model):
+    
+    name = m.CharField('Название', max_length=200)
+    start_date = m.DateTimeField('Дата старта')
+    end_date = m.DateTimeField('Дата окончания')
+    description = m.TextField('Описание', max_length=1000, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы' 
+        
+
+class Question(m.Model):
+    """
+    """
+    
+    QUESTION_TYPES = [
+        ('text_response', 'Ответ текстом'),
+        ('single_choice', 'Ответ с выбором одного варианта'),
+        ('multiple_choice', 'Ответ с выбором нескольких вариантов')
+    ]
+    
+    poll = m.ForeignKey(Poll, verbose_name='Опрос', on_delete=m.CASCADE)
+    text = m.TextField('Текст вопроса', max_length=200)
+    question_type = m.CharField(
+        'Тип вопроса',
+        max_length=15,
+        choices=QUESTION_TYPES,
+        default='single_choice'
+    )
+
+    def __str__(self):
+        return f'{self.poll} -- {self.text}'
+    
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+    
+
+class Answer(m.Model):
+    """
+    """
+    
+    question = m.ForeignKey(
+        Question,
+        verbose_name='Вопрос',
+        on_delete=m.CASCADE
+    )
+    text = m.CharField('Текст ответа', max_length=200)
+    
+    def __str__(self):
+        return f'{self.question} -- {self.text}'
+    
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
