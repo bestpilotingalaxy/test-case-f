21b00e8f65456df30d0792579edb6a2c165de8d5
bestpilotingalaxy <egorlaptev98@gmail.com>
2020-11-22 19:24:47 +0300

    WTF postgres??

--- polls_api/settings.py
+++ polls_api/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'polls_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER' : os.getenv('POSTGRES_USER'),
+        'PASSWORD' : os.getenv('POSTGRES_PASSWORD'),
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
